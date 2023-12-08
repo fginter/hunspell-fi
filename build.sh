08bb173cc49b41d9d1ce60fc859fa4ea2a16ec9e
Filip Ginter <filip.ginter@gmail.com>
2023-12-08 15:25:47 +0200

    bugfixes in scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cd prep-data
-zcat counts_min_3.raw.txt.gz | python3 filter.py | gzip > filtered_counts.txt.gz
-zcat filtered_counts.txt.gz | python3 prep_input.py > input.fi.txt
+cd data-prep
+zcat counts_min_3.raw.txt.gz | tqdm | python3 filter.py | gzip > filtered_counts.txt.gz
+zcat filtered_counts.txt.gz | tqdm | python3 prep_input.py > input.fi.txt
 cd ..
-java -jar hunspell-builder/target/hunspell-builder-0.2-jar-with-dependencies.jar prep-data/input.fi.txt spell-plugin/dictionaries/fi_FI
+java -jar hunspell-builder/target/hunspell-builder-0.2-jar-with-dependencies.jar data-prep/input.fi.txt spell-plugin/dictionaries/fi_FI
